cf0e93b0a4340545afa5cc394a2d183d41dbc72c
Isshiki111 <princeasirifibonsu@gmail.com>
2026-07-30 23:41:58 +0000

    Complete Assignment 1

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -35,5 +35,54 @@
 // =============================================================================
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+bool isPrime(int n)
+{
+    if (n < 2)
+    {
+        return false;
+    }
+
+    if (n == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    bool result = isPrime(number);
+
+    if (result)
+    {
+        cout << "\nNumber is prime";
+    }
+    else
+    {
+        cout << "\nNumber is not prime";
+    }
+
+    return 0;
+}
+
+
+    
+
+   
